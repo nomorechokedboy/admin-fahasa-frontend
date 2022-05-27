74b27ddc5755ff93e9dfcfff9d3b8dd91351a077
sekke276 <tringuyen2762001@gmail.com>
2022-05-27 08:50:46 +0700

    remove type/jest

--- cypress/component/reviewAPI.spec.ts
+++ cypress/component/reviewAPI.spec.ts
@@ -1,8 +1,7 @@
-import { getAllReviews, deleteEmployee } from '../../src/api';
-
+import { getAllReviews, deleteReview } from '../../src/api';
 import fakeDataReview from '../../src/api/review/mockData';
 
-describe('Employee api test', () => {
+describe('Review api test', () => {
   it('should return all Reviews', async () => {
     const data = await getAllReviews('/review');
     expect(data).length.to.be.greaterThan(0);
@@ -12,7 +11,7 @@ describe('Employee api test', () => {
   it('should delete review in the list', async () => {
     const deletedId = 'aaa11';
     const deletedList = fakeDataReview.filter((rev) => rev.id !== deletedId);
-    const data = await deleteEmployee(deletedId);
+    const data = await deleteReview('', deletedId);
     expect(data).length.to.be.greaterThan(0);
     expect(data).to.deep.eq(deletedList);
   });
