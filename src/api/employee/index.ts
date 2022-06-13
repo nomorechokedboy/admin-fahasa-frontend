442193a8c5253c4c07d23399b62354ed58e89944
sekke276 <tringuyen2762001@gmail.com>
2022-06-13 10:48:48 +0700

    comment mockdata employee

--- src/api/employee/index.ts
+++ src/api/employee/index.ts
@@ -1,6 +1,6 @@
 import axiosClient from '@/lib/axiosClient';
 import Employee from '@/types/employee';
-import fakeData from './mockData';
+// import fakeData from './mockData';
 
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
@@ -18,18 +18,18 @@ export const getEmployee = async (url: string, id: string) => {
   return res.data;
 };
 
-export const updateEmployee = async (employee: Employee) => {
-  await delay(1000);
-  const res = {
-    status: 200,
-    error: '',
-    data: fakeData.map((emp) =>
-      emp._id === employee._id ? { ...emp, ...employee } : emp,
-    ),
-  };
+// export const updateEmployee = async (employee: Employee) => {
+//   await delay(1000);
+//   const res = {
+//     status: 200,
+//     error: '',
+//     data: fakeData.map((emp) =>
+//       emp._id === employee._id ? { ...emp, ...employee } : emp,
+//     ),
+//   };
 
-  return res.data;
-};
+//   return res.data;
+// };
 
 export const deleteEmployee = async (url: string, id: string) => {
   const res = await axiosClient
